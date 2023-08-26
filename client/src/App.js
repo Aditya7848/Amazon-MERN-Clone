5677e471cc99ef39feb0b62297877919e2754293
Aditya7848 <adityasannidhanam@gmail.com>
2023-08-26 23:23:09 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/header/Navbar";
+import Newnav from "./components/newNav/Newnav";
+import Maincomp from "./components/home/Maincomp";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Newnav />
+      <Maincomp />
+    </>
+  );
+}
+
+export default App;
