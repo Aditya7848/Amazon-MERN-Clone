68b89b6494adca6bb290287dfd48c16dbcee22d3
Aditya7848 <adityasannidhanam@gmail.com>
2023-09-09 23:38:05 +0530

    added sign-in and sign-up pages

--- client/src/components/signup_signin/SignUp.js
+++ client/src/components/signup_signin/SignUp.js
@@ -1,7 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [uData, setUData] = useState({
+    fname: "",
+    email: "",
+    password: "",
+    mobile: "",
+    cpassword: "",
+  });
+  const handleData = (e) => {
+    const { name, value } = e.target;
+    setUData(() => {
+      return {
+        ...uData,
+        [name]: value,
+      };
+    });
+  };
+  return (
+    <>
+      <section>
+        <div className="sign_container">
+          <div className="sign_header">
+            <img src="./blacklogoamazon.png" alt="amazonLogo" />
+          </div>
+          <div className="sign_form">
+            <form action="">
+              <h1>Crate Account</h1>
+              <div className="form_data">
+                <label htmlFor="fname">Your name</label>
+                <input
+                  type="text"
+                  name="fname"
+                  id="fname"
+                  value={uData.fname}
+                  onChange={handleData}
+                />
+              </div>
+              <div className="form_data">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  value={uData.email}
+                  onChange={handleData}
+                />
+              </div>
+              <div className="form_data">
+                <label htmlFor="mobile">Mobile number</label>
+                <input
+                  type="tel"
+                  name="mobile"
+                  id="mobile"
+                  onChange={handleData}
+                  value={uData.mobile}
+                />
+              </div>
+              <div className="form_data">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={handleData}
+                  value={uData.password}
+                />
+              </div>
+              <div className="form_data">
+                <label htmlFor="cpassword">Password again</label>
+                <input
+                  type="password"
+                  name="cpassword"
+                  id="cpassword"
+                  onChange={handleData}
+                  value={uData.cpassword}
+                />
+              </div>
+              <button className="signin_btn">Continue</button>
+              <div className="signin_info">
+                <p>Already have an account</p>
+                <NavLink to="/login">Sign-in</NavLink>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default SignUp;
--- client/src/components/signup_signin/Signin.js
+++ client/src/components/signup_signin/Signin.js
@@ -1,8 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./signin.css";
+import { NavLink } from "react-router-dom";
 
 const Signin = () => {
-  return <div className="component">Signin</div>;
+  const [logData, setLogData] = useState({
+    email: "",
+    password: "",
+  });
+  function handleData(e) {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setLogData(() => {
+      return {
+        ...logData,
+        [name]: value,
+      };
+    });
+  }
+
+  return (
+    <>
+      <section>
+        <div className="sign_container">
+          <div className="sign_header">
+            <img src="./blacklogoamazon.png" alt="amazonLogo" />
+          </div>
+          <div className="sign_form">
+            <form action="">
+              <h1>Sign-in</h1>
+              <div className="form_data">
+                <label htmlFor="">Email</label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  value={logData.email}
+                  onChange={handleData}
+                />
+              </div>
+              <div className="form_data">
+                <label htmlFor="">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={logData.password}
+                  onChange={handleData}
+                />
+              </div>
+              <button className="signin_btn">Continue</button>
+            </form>
+          </div>
+          <div className="create_accountinfo">
+            <p>New to Amazon</p>
+            <NavLink to="/register">
+              <button>Create your amazon account</button>
+            </NavLink>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Signin;
