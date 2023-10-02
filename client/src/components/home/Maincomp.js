1bfcbf9685361bc3baa3ecfd6f4e953080b465df
Aditya7848 <adityasannidhanam@gmail.com>
2023-10-03 05:00:17 +0530

    react-redux rootreducers actions store

--- client/src/components/home/Maincomp.js
+++ client/src/components/home/Maincomp.js
@@ -1,8 +1,19 @@
 import Banner from "./Banner";
 import "./home.css";
 import Slide from "./Slide";
+import { getProducts } from "../redux/actions/action";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const Maincomp = () => {
+  const { products } = useSelector((state) => state.getproductsdata);
+  console.log(products);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
   return (
     <div className="home_section">
       <div className="banner_part">
@@ -10,7 +21,7 @@ const Maincomp = () => {
       </div>
       <div className="slide_part">
         <div className="left_slide">
-          <Slide title="Deal of the day" />
+          <Slide title="Deal of the day" products={products} />
         </div>
         <div className="right_slide">
           <h4>Festival latest launches</h4>
@@ -20,15 +31,15 @@ const Maincomp = () => {
           />
         </div>
       </div>
-      <Slide title="Today's deal" />
+      <Slide title="Today's deal" products={products} />
       <div className="center_img">
         <img
           src="https://cdn.zeebiz.com/sites/default/files/styles/zeebiz_850x478/public/2021/10/25/163614-happiness-daysamazon-india-twitter.jpg?itok=L44LO6yo"
           alt="diwali_special"
         />
       </div>
-      <Slide title="Bestsellers" />
-      <Slide title="Upto 80% off" />
+      <Slide title="Bestsellers" products={products} />
+      <Slide title="Upto 80% off" products={products} />
     </div>
   );
 };
